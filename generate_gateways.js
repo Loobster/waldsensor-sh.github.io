20be7a77a528673452c4ea0cbb184c4bab37b0e9
Loobster <deine@email.de>
2025-05-13 11:29:12 +0200

    🔄 karte-MINT.html erstellt am 2025-05-13 11:29

---
+++ generate_gateways.js
@@ -0,0 +1,31 @@
+const gateways = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.1,
+          54.3
+        ]
+      },
+      "properties": {
+        "name": "Test-Gateway 1"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.2,
+          54.32
+        ]
+      },
+      "properties": {
+        "name": "Test-Gateway 2"
+      }
+    }
+  ]
+};
