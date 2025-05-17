1c58c80d7fe18b550acc079568308db9092f3908
Loobster <deine@email.de>
2025-05-17 10:46:41 +0200

    Neu karte_MINT_FW_schulwald (Dateien gelöscht etc.)

---
+++ data/feuerwehren.js
@@ -0,0 +1,25 @@
+const feuerwehren = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [10.1, 54.32]
+      },
+      "properties": {
+        "name": "Freiwillige Feuerwehr Kiel"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [9.6, 54.52]
+      },
+      "properties": {
+        "name": "Freiwillige Feuerwehr Schleswig"
+      }
+    }
+  ]
+};
---
+++ data/schulwaelder.js
@@ -0,0 +1,25 @@
+const schulwaelder = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [9.985, 54.3]
+      },
+      "properties": {
+        "name": "Schulwald Kiel-Ost"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [9.52, 54.48]
+      },
+      "properties": {
+        "name": "Schulwald Schleswig"
+      }
+    }
+  ]
+};
