0fa2038c25818850f56ba6cc7ffff19ad08f603e
Loobster <deine@email.de>
2025-05-18 19:22:26 +0200

    💬 schulwaelder.js neu

--- data/schulwaelder.js
+++ data/schulwaelder.js
@@ -5,20 +5,126 @@ const schulwaelder = {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [9.985, 54.3]
+        "coordinates": [
+          9.8260859,
+          54.364641
+        ]
       },
       "properties": {
-        "name": "Schulwald Kiel-Ost"
+        "name": "Sehestedt (Kreis Rendsburg-Eckernförde)",
+        "typ": "🌳 Schulwald"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [9.52, 54.48]
+        "coordinates": [
+          8.79619436832986,
+          54.70468409395008
+        ]
       },
       "properties": {
-        "name": "Schulwald Schleswig"
+        "name": "Fahretofter Schulwald",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.72794,
+          54.4712227
+        ]
+      },
+      "properties": {
+        "name": "Grundschule Surendorf Schwedeneck",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.51667,
+          54.31667
+        ]
+      },
+      "properties": {
+        "name": "Giekauf Schule Seekrug am Selenter See",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.5102163,
+          54.3518551
+        ]
+      },
+      "properties": {
+        "name": "Schulwald Tetenhusen",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.07810523559398,
+          54.183525425482294
+        ]
+      },
+      "properties": {
+        "name": "Schulwald Brügge",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.4282403,
+          53.4107902
+        ]
+      },
+      "properties": {
+        "name": "Waldschule Geesthacht in Grünhof-Tesperhude",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.497381773182523,
+          53.83772505731196
+        ]
+      },
+      "properties": {
+        "name": "Schulwald Reinfeld",
+        "typ": "🌳 Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.553678987596994,
+          54.53009521099136
+        ]
+      },
+      "properties": {
+        "name": "Schulwald Schleswig",
+        "typ": "🌳 Schulwald"
       }
     }
   ]
