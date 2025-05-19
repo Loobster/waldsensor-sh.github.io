27d24463e3a60e226c50fdb6796f741060fffd95
Loobster <deine@email.de>
2025-05-19 17:58:13 +0200

    🆕 schulwaelder.js geändert hinzugefügt

--- data/schulwaelder.js
+++ data/schulwaelder.js
@@ -1,4 +1,252 @@
 const schulwaelder = {
   "type": "FeatureCollection",
-  "features": []
-}
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.840618866666667,
+          54.16660653333333
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.497649999999997,
+          53.83776480952382
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.674345953333333,
+          53.740985159999994
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.510338527272726,
+          54.35227536363636
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.6565767,
+          54.28324575454545
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.467488957142859,
+          54.80192932142857
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.976976414285716,
+          53.876042628571426
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.717258300000001,
+          54.306866750000005
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.121174611111112,
+          54.2208919
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.55664846153846,
+          53.437513023076924
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.04907937,
+          54.27238129
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.064287654545454,
+          54.492702027272735
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.770964792857143,
+          54.06815317857143
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.240254054545453,
+          54.460071772727275
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.66242867,
+          54.22962199999999
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.222515920000001,
+          53.58344115999999
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.963535762500001,
+          54.39734818749999
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.1421551,
+          53.78620476
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.206690214285715,
+          53.964806599999996
+        ]
+      },
+      "properties": {
+        "name": "Schulwald"
+      }
+    }
+  ]
+};
