da3e78653156c2e6759cf2321ec1637d739b25db
Loobster <deine@email.de>
2025-05-20 11:37:06 +0200

    🆕 schulwaelder.js geändert hinzugefügt

---
+++ data/schulwaelder_mit_popup.js
@@ -0,0 +1,309 @@
+const schulwaelder = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.838455820820338,
+          54.165322061487885
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.16532, 9.83846<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.497931191911881,
+          53.837800040679184
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.83780, 10.49793<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.674405187866641,
+          53.74103990974692
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.74104, 9.67441<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.509970264587682,
+          54.352304319797724
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.35230, 9.50997<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.656596312574777,
+          54.283088455988434
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.28309, 10.65660<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.46734737486769,
+          54.80173151125864
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.80173, 9.46735<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.977010332230435,
+          53.876021895956605
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.87602, 9.97701<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.717237033333332,
+          54.3067621
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.30676, 9.71724<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.121329183259432,
+          54.22089702200187
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.22090, 9.12133<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.556762212382653,
+          53.43751874289368
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.43752, 10.55676<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.049142276188983,
+          54.27234390944349
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.27234, 9.04914<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.064587477462918,
+          54.492911970489885
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.49291, 9.06459<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.7713903121004,
+          54.06833831988231
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.06834, 10.77139<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.24030532628009,
+          54.46007614149905
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.46008, 9.24031<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.662530314856221,
+          54.229600859548135
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.22960, 9.66253<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.222521005597809,
+          53.583216102078126
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.58322, 10.22252<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.963465965743316,
+          54.39732931509425
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 54.39733, 9.96347<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.142153272536676,
+          53.786292891127154
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.78629, 10.14215<br><i>Schulname noch ergänzen</i>"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          10.207064182441327,
+          53.96497257129481
+        ]
+      },
+      "properties": {
+        "name": "🟢 Schulwald",
+        "beschreibung": "Schulname noch ergänzen",
+        "typ": "Polygonfläche",
+        "popup": "<b>🟢 Schulwald</b><br>Typ: Polygonfläche<br>Koordinaten: 53.96497, 10.20706<br><i>Schulname noch ergänzen</i>"
+      }
+    }
+  ]
+};
