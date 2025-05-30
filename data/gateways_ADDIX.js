dabe68e13aa8723bba52f231fdf660b69de5df65
Loobster <deine@email.de>
2025-05-30 06:30:03 +0200

    🔄 Auto-Update: Gateways aktualisiert am 2025-05-30 06:30

--- data/gateways_ADDIX.js
+++ data/gateways_ADDIX.js
@@ -14,7 +14,7 @@ const gateways_addix = {
         "id": "eui-24e124fffef36abf-v2",
         "name": "adx-sh-021",
         "alt": 68,
-        "updated": "2025-05-29T04:26:06.014047Z"
+        "updated": "2025-05-30T04:28:43.553532Z"
       }
     },
     {
@@ -30,7 +30,7 @@ const gateways_addix = {
         "id": "eui-24e124fffef37fe12",
         "name": "adx-sh-098",
         "alt": 30,
-        "updated": "2025-05-29T04:23:03.88408Z"
+        "updated": "2025-05-30T04:23:01.588204Z"
       }
     },
     {
@@ -46,7 +46,7 @@ const gateways_addix = {
         "id": "eui-24e124fffef38749",
         "name": "adx-sh-128",
         "alt": 15,
-        "updated": "2025-05-29T04:23:25.568346Z"
+        "updated": "2025-05-30T04:22:41.644314Z"
       }
     },
     {
@@ -62,7 +62,7 @@ const gateways_addix = {
         "id": "eui-24e124fffef38f11-2",
         "name": "adx-sh-093",
         "alt": 30,
-        "updated": "2025-05-29T04:20:41.646109Z"
+        "updated": "2025-05-30T04:22:55.312648Z"
       }
     },
     {
@@ -78,7 +78,7 @@ const gateways_addix = {
         "id": "eui-24e124fffef39289",
         "name": "adx-sh-103",
         "alt": 27,
-        "updated": "2025-05-29T04:27:45.626569Z"
+        "updated": "2025-05-30T04:24:21.881598Z"
       }
     },
     {
@@ -94,7 +94,7 @@ const gateways_addix = {
         "id": "eui-24e124fffef39695",
         "name": "adx-sh-019",
         "alt": 25,
-        "updated": "2025-05-29T04:21:47.798914Z"
+        "updated": "2025-05-30T04:22:51.830823Z"
       }
     }
   ]
